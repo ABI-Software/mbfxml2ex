d9900ed885e1a052ffe6aa17fb509f41aad121fd
Richard Christie <r.christie@auckland.ac.nz>
2023-04-13 10:01:30 +1200

    Migrate to cmlibs

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if not version:
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-dependencies = ['opencmiss.zinc >= 3.5.1', 'opencmiss.utils >= 0.4.0']
+dependencies = ['cmlibs.zinc >= 4.0.0', 'cmlibs.utils >= 0.4.0']
 
 test_deps = [
     'coverage',
@@ -52,8 +52,8 @@ setup(
         ]
     },
     license="Apache Software License",
-    keywords="MBF OpenCMISS-Zinc",
-    url="https://github.com/hsorby/mbfxml2ex",
+    keywords="MBF CMLibs-Zinc",
+    url="https://github.com/ABI-Software/mbfxml2ex",
     download_url="",
     classifiers=[
         "Development Status :: 3 - Alpha",
